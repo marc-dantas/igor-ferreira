a3962d73cb0ff960d7f564177671744558d9e1e3
marc-dantas <marciodantasms@hotmail.com>
2023-05-29 17:28:58 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import 'bulma';
+
+function App() {
+  return (
+    <main className="hero is-link">
+      <div className="hero-body">
+        <div className="box">
+          Hello, Igor Ferreira!
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App;
